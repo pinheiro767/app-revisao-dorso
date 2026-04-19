7dcde13e7c065849f1c9abcfa9fbb6867c2208e5
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-19 04:23:37 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,3 @@
-const CACHE_NAME = 'atlas-miologia-funcionando-v1';
-const URLS_TO_CACHE = ['./','./index.html','./manifest.json','./assets/icons/icon192.png','./assets/icons/icon512.png'];
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./index.html'))));
-});
+
+self.addEventListener('install',e=>self.skipWaiting())
+self.addEventListener('fetch',()=>{})
